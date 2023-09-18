3aec5f5e4dc4099d0945e5711af80548ccd0db21
Doppler <doppler@doppler.HP.Pavillon>
2023-09-18 19:16:16 +0200

    arXiv can be searched, just not for companies

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # PaperPulse
 
-This aim of this project is to have a script editing regularly a personnal database with new research papers, using differents keywords.
+This aim of this project is to have a script editing regularly a personnal database with new research papers, using differents keywords. When I'll be satisfied with the scope of publishers, I will create a way to store the search results in a proper database.
 
 ## Current scope 
 
 | **Current publishers**  |  
 |-------------------------|
 | HAL                     |   
+| arXiv                   |
 
+## Current ways to find papers
+The system takes all keywords from a yaml file, and search for them with AND operators
 
 | **Ways to find papers**            |
 |------------------------------------|
@@ -16,6 +19,13 @@ This aim of this project is to have a script editing regularly a personnal datab
 | Authors' names                     |
 | Companies the authors publish from |
 
+## Futur archives that will be in the project
+
 Futur publishers :
-- arXiv
 - papers with code
+
+I'm open to suggestions to be able to search
+
+
+
+Thank you to arXiv for use of its open access interoperability
--- getPaper.py
+++ getPaper.py
@@ -1,53 +1,88 @@
 import requests
 import yaml
 from yaml.loader import SafeLoader
-from utils import  printJSON
-
-with open('keywords.yaml') as f:
-    data=yaml.load(f, Loader=SafeLoader)
-
-title = data['title']
-authors = data['authors']
-abstract = data['abstract']
-enterprise = data['enterprise']
+import feedparser
+from utils import *
 
 
-headers = {'Accept' : 'application/json'}
-base_url = ' http://api.archives-ouvertes.fr/search/?q='
-end_url = '&rows=2&wt=json'
 
-url=''
+def getHAL(yaml_url): # returns a list of articles, each in the form of a dict. See utils.printJSON to use the result
+    with open(yaml_url) as f:
+        data=yaml.load(f, Loader=SafeLoader)
 
-url=url+'title_t:('
-for t in title :
-    if t is not None:
-        url=url+t+' '
-url=url+')'
+    title = data['title']
+    authors = data['authors']
+    abstract = data['abstract']
+    enterprise = data['enterprise']
+    headers = {'Accept' : 'application/json'}
+    base_url = ' http://api.archives-ouvertes.fr/search/?q='
+    end_url = '&rows=50&wt=json'
 
+    url=''
 
-url=url+'&abstract_s:()'
-for abs in abstract:
-    if abs is not None:
-        url=url+abs+' '
-url=url+')'
-
-
-url=url+'&auth_t:('
-for aut in authors:
-    if aut is not None:
-        url=url+aut+' '
-url=url+')'
+    url=url+'title_t:('
+    for t in title :
+        if t is not None:
+            url=url+t+' '
+    url=url+')'
 
 
-url=url+'&authEmail_s:('
-for ent in enterprise :
-    if ent is not None:
-        url=url+ent+' '
-url=url+')'
+    url=url+'&abstract_s:()'
+    for abs in abstract:
+        if abs is not None:
+            url=url+abs+' '
+    url=url+')'
 
-url=base_url+url+end_url
-r=requests.get(url,headers=headers)
-r.raise_for_status()
-if r.status_code != 204:
-    #print(r.json())
-    printJSON(r.json())
+
+    url=url+'&auth_t:('
+    for aut in authors:
+        if aut is not None:
+            url=url+aut+' '
+    url=url+')'
+
+
+    url=url+'&authEmail_s:('
+    for ent in enterprise :
+        if ent is not None:
+            url=url+ent+' '
+    url=url+')'
+
+    url=base_url+url+end_url
+    r=requests.get(url,headers=headers)
+    r.raise_for_status()
+    if r.status_code != 204:
+        #print(r.json())
+        printJSON(r.json())
+
+
+def getArXiv(yaml_url):
+    with open(yaml_url) as f:
+        data=yaml.load(f, Loader=SafeLoader)
+    title = data['title']
+    authors = data['authors']
+    abstract = data['abstract']
+
+    list=title+authors+abstract
+
+    url='http://export.arxiv.org/api/query?search_query='
+    end_url = '&max_results=4'
+
+    for i in range(len(list)):
+        key=list[i]
+        if key is not None:
+            if url[-1]!='=': #i.e. if we are not the first item in the list
+                url=url+'+AND+'
+            url=url+'all:'+key
+
+    url = url + end_url
+
+    feed = feedparser.parse(url)
+    for article in feed['entries']:
+        print(f"Article name : {article['title']}")
+        name_list = ''
+        for name in article['authors']:
+            name_list=name_list+name['name']+', '
+        print(f"List of authors : {name_list}")
+        print(f"URL : {article['links'][1]['href']}\n")
+        
+getArXiv('keywords.yaml')
