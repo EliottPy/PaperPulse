0240e117ac3cdfa4eee11bc88a25d46504cc4797
Doppler <doppler@doppler.HP.Pavillon>
2023-09-20 11:34:40 +0200

    Fix bug where keywords for arXiv were used for every field

--- getPaper.py
+++ getPaper.py
@@ -62,17 +62,30 @@ def getArXiv(yaml_url):
     authors = data['authors']
     abstract = data['abstract']
 
-    list=title+authors+abstract
 
     url='http://export.arxiv.org/api/query?search_query='
     end_url = '&max_results=4'
 
-    for i in range(len(list)):
-        key=list[i]
+    for i in range(len(title)):
+        key=title[i]
         if key is not None:
             if url[-1]!='=': #i.e. if we are not the first item in the list
                 url=url+'+AND+'
-            url=url+'all:'+key
+            url=url+'ti:'+key
+
+    for i in range(len(authors)):
+        key=authors[i]
+        if key is not None:
+            if url[-1]!='=': #i.e. if we are not the first item in the list
+                url=url+'+AND+'
+            url=url+'au:'+key
+
+    for i in range(len(abstract)):
+        key=abstract[i]
+        if key is not None:
+            if url[-1]!='=': #i.e. if we are not the first item in the list
+                url=url+'+AND+'
+            url=url+'abs:'+key
 
     url = url + end_url
 
@@ -86,3 +99,4 @@ def getArXiv(yaml_url):
         print(f"URL : {article['links'][1]['href']}\n")
         
 getArXiv('keywords.yaml')
+getHAL('keywords.yaml')
